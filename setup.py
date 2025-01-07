ebf534caa7c6222293dd3d0c1808469504ce6e03
Valentin Lafage <vlafage.dev@free.fr>
2025-01-07 11:10:57 +0100

    chore: set up project skeleton :tada:
    resolves #1
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+directory = Path(__file__).resolve().parent
+with open(directory / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+    
+setup(
+    name="dotfiles",
+    version="0.0.1",
+    author="Valentin Lafage",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "dotfiles = cli.__main__:main",
+        ],
+    },
+)
