9b2f030213bce092311eda97fed16237de0c7f34
Valentin Lafage <vlafage.dev@free.fr>
2025-01-07 14:17:25 +0100

    chore: add logs
    resolves #3
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

---
+++ src/config/__init__.py
@@ -0,0 +1,6 @@
+from .paths import (
+    ROOT_DIR,
+    HOME_DIR,
+    LOG_DIR,
+)
+from .logging import set_up_root_logger
---
+++ src/config/paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parents[2]
+HOME_DIR = Path.home()
+
+LOG_DIR = ROOT_DIR / "logs"
