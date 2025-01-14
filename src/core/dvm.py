519b8fdd67d1857831cf106427fb86cda47b2631
Valentin Lafage <vlafage.dev@free.fr>
2025-01-14 11:12:56 +0100

    chore: rename project to dvm
    resolves #23
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

--- src/core/dvm.py
+++ src/core/dvm.py
@@ -2,7 +2,7 @@ from config import set_up_root_logger
 from core.services import FileService, SymlinkService
 
 
-class Dotfiles:
+class Dvm:
     _instance = None
 
     @property
@@ -11,17 +11,17 @@ class Dotfiles:
 
     def __new__(cls):
         if cls._instance is not None:
-            raise RuntimeError("An instance of Dotfiles already exists!")
+            raise RuntimeError("An instance of Dvm already exists!")
         cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
-        Dotfiles._instance = self
+        Dvm._instance = self
         self._logger = set_up_root_logger()
         self._symlink_service = SymlinkService(FileService())
 
     @classmethod
     def get_instance(cls):
         if cls._instance is None:
-            raise RuntimeError("Dotfiles has not been initialized!")
+            raise RuntimeError("Dvm has not been initialized!")
         return cls._instance
