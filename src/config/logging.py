aed1ca5668f5e526f8b3e16c29c3f1ebd2afb8cd
Valentin Lafage <vlafage.dev@free.fr>
2025-01-14 10:49:30 +0100

    fix: set up root logger using config file
    resolves #16
    
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

--- src/config/logging.py
+++ src/config/logging.py
@@ -1,33 +1,11 @@
 import os
 import logging
+import logging.config
 
-from config import LOGS_DIR
-
-FMT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-DATE_FMT = "%Y-%m-%d %H:%M:%S"
+from config import ROOT_DIR, LOGS_DIR
 
 
 def set_up_root_logger() -> logging.Logger:
     os.makedirs(LOGS_DIR, exist_ok=True)
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-
-    logger.addHandler(create_file_handler())
-    logger.addHandler(create_console_handler())
-
-    return logger
-
-
-def create_file_handler() -> logging.Handler:
-    handler = logging.FileHandler(os.path.join(LOGS_DIR, "dotfiles.log"))
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(logging.Formatter(FMT, datefmt=DATE_FMT))
-    return handler
-
-
-def create_console_handler():
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.WARNING)
-    handler.setFormatter(logging.Formatter(FMT, datefmt=DATE_FMT))
-    return handler
+    logging.config.fileConfig(f"{ROOT_DIR}/logging.conf")
+    return logging.getLogger()
